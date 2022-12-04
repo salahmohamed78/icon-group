9a9a7cce44076269ed32caaa958d361d6f06a083
Salah Mohamed <salahmohamedabdelhalim@gmail.com>
2022-12-04 21:12:08 +0200

    Initial commit

---
+++ js/header.js
@@ -0,0 +1,65 @@
+/*
+                                                                         بسم الله الرحمن الرحيم                                                                    
+Created by: SALAH MOHAMED
+
+*/
+
+/*functions */
+
+let btn = document.getElementById("menuBtn");
+let nav = document.getElementById("nav");
+let isExpanded = false;
+
+btn.addEventListener("click", function () {
+  btn.classList.toggle("menuBtn--close");
+  if (isExpanded) {
+    nav.classList.toggle("nav--open");
+    setTimeout(function () {
+      nav.style.display = "none";
+    }, 800);
+    isExpanded = false;
+  } else {
+    nav.style.display = "block";
+    setTimeout(function () {
+      nav.classList.toggle("nav--open");
+    }, 10);
+    isExpanded = true;
+  }
+});
+
+//           companies
+
+let companiesBtn = document.getElementById("comBtn");
+let companiesList = document.getElementById("comList");
+let dropDownBtn = document.getElementById("dropDownBtn");
+let isDown = false;
+
+companiesBtn.addEventListener("click", function () {
+  dropDownBtn.classList.toggle("dropUp");
+  if (isDown) {
+    companiesList.classList.toggle("campanies__list--show");
+
+    setTimeout(function () {
+      companiesList.style.display = "none";
+    }, 800);
+    isDown = false;
+  } else {
+    companiesList.style.display = "block";
+    setTimeout(function () {
+      companiesList.classList.toggle("campanies__list--show");
+    }, 1);
+    isDown = true;
+  }
+});
+
+function setMenuHeight() {
+  let menuHeight = document.getElementById("header").offsetHeight;
+
+  document
+    .querySelector(":root")
+    .style.setProperty("--menuHeight", `${menuHeight}px`);
+}
+setMenuHeight();
+
+window.addEventListener("DOMContentLoaded", setMenuHeight);
+window.addEventListener("resize", setMenuHeight);
---
+++ js/main.js
@@ -0,0 +1,135 @@
+/*
+                                                                         بسم الله الرحمن الرحيم                                                                    
+Created by: SALAH MOHAMED
+number = 01220000538
+*/
+
+AOS.init({
+  offset: 150,
+  duration: 1000,
+  once: true,
+});
+
+/*
+
+                                            swiper
+
+
+*/
+
+var swiper = new Swiper("#icon__swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  speed: 750,
+
+  preloadImages: false,
+  lazy: true,
+  watchSlidesProgress: true,
+
+  // Default parameters
+  slidesPerView: 1.5,
+  spaceBetween: 0,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    575: {
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+    },
+    // when window width is >= 480px
+    1000: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    // when window width is >= 640px
+    1400: {
+      slidesPerView: 2.5,
+      spaceBetween: 0,
+    },
+  },
+
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 200,
+    modifier: 0.7,
+    slideShadows: true,
+  },
+});
+
+let projectsSwiper = new Swiper(".projectSwiper", {
+  loop: true,
+  preloadImages: false,
+  lazy: true,
+  grabCursor: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+let gallerySwiper = new Swiper(".gallerySwiper", {
+  preloadImages: false,
+  lazy: true,
+
+  grabCursor: true,
+  slidesPerView: 1,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    700: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      watchSlidesProgress: true,
+    },
+    // when window width is >= 480px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      watchSlidesProgress: true,
+    },
+  },
+
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let singleSlider = new Swiper(".singleSwiper", {
+  preloadImages: false,
+  lazy: true,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
